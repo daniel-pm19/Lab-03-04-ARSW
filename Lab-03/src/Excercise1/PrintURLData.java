d297c716d4826f08dff927785a76715ae2945f91
Daniel <daniel.patino-m@mail.escuelaing.edu.co>
2026-02-17 23:41:47 -0500

    add(core): Excercise 1 and 2

---
+++ Lab-03/src/Excercise1/PrintURLData.java
@@ -0,0 +1,38 @@
+package Excercise1;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class PrintURLData {
+    public static void main (String[] args){
+        
+        try {
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter the URL: ");
+            String newURL = sc.nextLine();
+
+            URL enteredURL = new URL(newURL);
+            
+            System.out.println( enteredURL + " Data Results");
+            System.out.println("");
+            System.out.println("URL Protocol: " + enteredURL.getProtocol());
+            System.out.println("URL Authority: " + enteredURL.getAuthority());
+            System.out.println("URL Host: " + enteredURL.getHost());
+            System.out.println("URL Port: " + enteredURL.getPort());
+            System.out.println("URL Path: " + enteredURL.getPath());
+            System.out.println("URL Query: " + enteredURL.getQuery());
+            System.out.println("URL File: " + enteredURL.getFile());
+            System.out.println("URL Reference: " + enteredURL.getRef());
+
+        } catch (MalformedURLException e){
+
+            e.printStackTrace();
+
+        }
+    }
+
+}
---
+++ Lab-03/src/Excercise2/WebSiteReader.java
@@ -0,0 +1,53 @@
+package Excercise2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+public class WebSiteReader {
+    public static void main(String[] args){
+        try {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Enter the URL: ");
+            String newURL = sc.nextLine();
+
+            URL enteredURL = new URL(newURL);
+            List<String> result = new ArrayList<>();
+
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(enteredURL.openStream()))){
+                String inputLine = null;
+                
+                while((inputLine = reader.readLine()) != null){
+                    result.add(inputLine);
+                    System.out.println(inputLine);
+                }
+
+            } catch (IOException x){
+                System.err.println(x);
+            }
+
+            try(BufferedWriter writer = new BufferedWriter(new FileWriter("./src/Excercise2/result.html"))) {
+                for(String l : result){
+                    writer.write(l);
+                    writer.newLine();
+                }
+            } catch (IOException y){
+                System.err.println(y);
+            }
+
+
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+    }
+}
